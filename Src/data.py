895a72dd44af6516fe2d950c424f4c7dae40da1c
august <boland.a@husky.neu.edu>
2024-04-15 23:26:01 -0400

    made a data class so that you can start using the dataframe without worrying about changes I make in the future.

--- Src/data.py
+++ Src/data.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+##Use this class. 
+##Access all the data with access so I can make changes without breaking
+##whatever functions you make.
+
+class Data:
+    
+    def __init__(self):
+        self.data = pd.read_csv('data.csv')
+        self.df = pd.DataFrame(self.data)
+
+    def access(self, column, row):
+        return self.df[column][row]
