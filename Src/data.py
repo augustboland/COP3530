aeb175f938be007f33d06db3fbdf4be9c8041ccf
August Boland <40546359+augustboland@users.noreply.github.com>
2024-04-22 09:09:44 -0400

    Update data.py

--- Src/data.py
+++ Src/data.py
@@ -13,12 +13,12 @@ class Data:
     def access(self, column, row):
         return self.df[column][row]
     #From stackoverflow on dataframes.
-    def add_rank_to_dataframe(df, column):
-    # Copy the specific column to a new list
-    arr = df[column].tolist()
-    # Sort the list using Radix Sort
-    radix_sort(arr)
-    # Create a rank dictionary
-    rank_dict = {value: x + 1 for x, value in enumerate(arr)}
-    # Map the ranks back to the DataFrame
-    df[column+'Rank'] = df[column].map(rank_dict)
+    def radix_sort(df, column):
+        # Copy the specific column to a new list
+        arr = df[column].tolist()
+        # Sort the list using Radix Sort
+        radix_sort(arr)
+        # Create a rank dictionary
+        rank_dict = {value: x + 1 for x, value in enumerate(arr)}
+        # Map the ranks back to the DataFrame
+        df[column+'Rank'] = df[column].map(rank_dict)
